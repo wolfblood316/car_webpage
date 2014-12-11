36bc3b38930c50c78ef233552d0540ee2ace562b
wolfblood316 <adrianlandin@yahoo.com>
2014-12-11 15:41:20 -0500

    added images

--- index.php
+++ index.php
@@ -25,6 +25,53 @@
         
       </div>
     </div>-->
+      <div id="myCarousel" class="carousel slide">
+
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src ="images/car.jpg" class="img-responsive img-center " width="100%">
+      <div class="container">
+        <div class="carousel-caption">
+       
+          
+          
+       
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/car4.jpg" class="img-responsive img-center "  width="100%">
+      <div class="container">
+        <div class="carousel-caption">
+         
+          
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/car3.jpg" class="img-responsive img-center"  width="100%">
+      <div class="container">
+        <div class="carousel-caption">
+         
+         
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="icon-prev"></span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="icon-next"></span>
+  </a>  
+</div>
+
   <div class="container">
       <!-- Example row of columns -->
       <div class="row">
